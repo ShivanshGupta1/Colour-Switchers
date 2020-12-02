870999fb86b25a9a09225931f351f842dd9e6a37
ShivanshGupta1 <68952452+ShivanshGupta1@users.noreply.github.com>
2020-12-02 09:30:44 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function colourchange() {
+    if (document.body.style.backgroundColor=="aqua") {
+        document.body.style.backgroundColor="brown"
+    }
+    else if (document.body.style.backgroundColor=="brown") {
+        document.body.style.backgroundColor="aqua"
+    }
+}
